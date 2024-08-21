f663671371d48fc2492f06ca9da89050b96588eb
Uday Lad <udaylad77@gmail.com>
2024-08-21 09:01:37 +0530

    Update - added few data in skills list in Day20

--- lib/day20/screens/profile_app.dart
+++ lib/day20/screens/profile_app.dart
@@ -9,6 +9,23 @@ class ProfileApp extends StatelessWidget {
     'Traveling',
     'Cooking',
     'Reading',
+    'Surfing Web',
+    'Tumblr',
+    'Pinterest',
+    'Instagram',
+    'Facebook',
+    'Twitter',
+    'LinkedIn',
+    'YouTube',
+    'Snapchat',
+    'WhatsApp',
+    'Telegram',
+    'Skype',
+    'Discord',
+    'Slack',
+    'Google Hangouts',
+    'Zoom',
+    'Skype',
   ];
   ProfileApp({super.key});
   @override
@@ -19,33 +36,36 @@ class ProfileApp extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         // Allows scrolling if content overflows
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 20),
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage('https://via.placeholder.com/150'),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'John Doe',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Software Developer',
-              style: TextStyle(fontSize: 18, color: Colors.grey[700]),
-            ),
-            SizedBox(height: 20),
-            // ListView of skills/hobbies
-            ...skills
-                .map((skill) => ListTile(
-                      title: Text(skill),
-                      leading: Icon(Icons.star,
-                          color: Colors.blue), // Optional: Adds an icon
-                    ))
-                .toList(),
-          ],
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              CircleAvatar(
+                radius: 80,
+                backgroundImage:
+                    NetworkImage('https://via.placeholder.com/500'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Uday Lad',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Mobile App Developer',
+                style: TextStyle(fontSize: 18, color: Colors.grey[700]),
+              ),
+              SizedBox(height: 20),
+              // ListView of skills/hobbies
+              ...skills
+                  .map((skill) => ListTile(
+                        title: Text(skill),
+                        leading: Icon(Icons.star,
+                            color: Colors.blue), // Optional: Adds an icon
+                      ))
+                  .toList(),
+            ],
+          ),
         ),
       ),
     );
